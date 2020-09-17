d6db7efb73b4fb4172c979e4d4d7248296b3554b
Edith <esantiago808@gmail.com>
2020-09-16 16:01:57 -1000

    Week 2 Node Callback Hell and Promises

--- operations.js
+++ operations.js
@@ -1,38 +1,19 @@
-const assert = require('assert').strict;
-
-//analogous to CRUD, create = insert, read = insert, update = update, delete = remove
-
-exports.insertDocument = (db, document, collection, callback) => {
-    const coll = db.collection(collection);     //example of collection (table) campsites, partners
-
-    coll.insertOne(document, (err, result) => {
-        assert.strictEqual(err, null);        //this is the err part of the argument in the line above
-        callback(result);                        //the callback will return from index.js
-    });
+exports.insertDocument = (db, document, collection) => {
+    const coll = db.collection(collection); 
+    return coll.insertOne(document);
 };
 
-exports.findDocuments = (db, collection, callback) => {         //list all the documents
+exports.findDocuments = (db, collection) => { 
     const coll = db.collection(collection);
-
-    coll.find().toArray((err, docs) => {           //find all documents and list in an array
-        assert.strictEqual(err, null);
-        callback(docs);
-    });
+    return coll.find().toArray();
 };
 
-exports.removeDocument = (db, document, collection, callback) => {
+exports.removeDocument = (db, document, collection) => {
     const coll = db.collection(collection);
-    coll.deleteOne(document, (err, result) => {
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.deleteOne(document);
 };
 
-exports.updateDocument = (db, document, update, collection, callback) => {
+exports.updateDocument = (db, document, update, collection) => {
     const coll = db.collection(collection);
-
-    coll.updateOne(document, { $set: update }, null, (err, result) => {        //update requires 4 parameters
-        assert.strictEqual(err, null);
-        callback(result);
-    });
+    return coll.updateOne(document, { $set: update }, null);
 };
